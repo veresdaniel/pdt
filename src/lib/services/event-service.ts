41b488a091d4e563aaeabb39f0ef9b7fc1273ede
Marton Lajos <lajosmarton@hotmail.com>
2026-05-25 20:17:50 +0200

    fixed local SSL cert error

--- src/lib/services/event-service.ts
+++ src/lib/services/event-service.ts
@@ -1,20 +1,22 @@
 import { API_BASE_URL } from '$env/static/private';
-import type { EventModel } from "$lib/models/event-model.model";
+import type { EventModel } from '$lib/models/event-model.model';
 
 export class EventService {
+  async getUpcomingEvent(
+    fetch: typeof global.fetch,
+    lang: string
+  ): Promise<EventModel> {
+    const res: Response = await fetch(`${API_BASE_URL}/events/upcoming`, {
+      headers: {
+        'x-language': lang
+      }
+    });
 
-    async getUpcomingEvent(
-        fetch: typeof global.fetch,
-        lang: string
-    ): Promise<EventModel> {
-        const res: Response = await fetch(`${API_BASE_URL}/events/upcoming`, {
-            headers: {
-                'x-language': lang
-            }
-        });
+    if (!res.ok)
+      throw new Error(
+        `Failed to fetch event url: ${JSON.stringify(await res.json())}`
+      );
 
-        if (!res.ok) throw new Error(`Failed to fetch event url: ${JSON.stringify(await res.json())}`);
-
-        return res.json();
-    }
+    return res.json();
+  }
 }
