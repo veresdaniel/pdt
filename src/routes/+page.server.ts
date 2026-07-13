d3ccb9c4a0f61568c16e94c8489bd6115dae7312
Marton Lajos <lajosmarton@hotmail.com>
2026-07-13 23:40:31 +0200

    unnecessary mocks removed, language switch fixed, npm packages updated

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -4,17 +4,25 @@ import { EventService } from '$lib/services/event-service';
 import type { Locale } from '$lib/i18n/i18n';
 
 const eventService = new EventService();
-export const load: PageServerLoad = async ({ fetch, locals, request }) => {
+export const load: PageServerLoad = async ({
+  fetch,
+  locals,
+  request,
+  cookies
+}) => {
+  const storedLanguage = cookies.get('language');
+  const lang: Locale =
+    storedLanguage === 'hu' || storedLanguage === 'en'
+      ? storedLanguage
+      : (() => {
+          const acceptLanguage: string =
+            request.headers.get('accept-language') ?? '';
+          const languages = acceptLanguage
+            .split(',')
+            .map(l => l.split(';')[0].trim().toLowerCase());
 
-  const acceptLanguage: string = request.headers.get('accept-language') ?? '';
-
-  const languages = acceptLanguage
-    .split(',')
-    .map((l) => l.split(';')[0].trim().toLowerCase());
-
-  const lang: Locale = languages.some((l) => l.startsWith('hu'))
-    ? 'hu'
-    : 'en';
+          return languages.some(l => l.startsWith('hu')) ? 'hu' : 'en';
+        })();
 
   throw redirect(302, `/${lang}/events`);
 };
--- src/routes/[lang=lang]/+page.server.ts
+++ src/routes/[lang=lang]/+page.server.ts
@@ -4,10 +4,7 @@ import { EventService } from '$lib/services/event-service';
 import type { Locale } from '$lib/i18n/i18n';
 
 const eventService = new EventService();
-export const load: PageServerLoad = async ({ params, fetch, locals }) => {
+export const load: PageServerLoad = async ({ params }) => {
   const lang = params.lang as Locale;
-  const upcomingEvent = await eventService.getUpcomingEvent(fetch, lang);
-  
-  locals.upcomingEvent = upcomingEvent;
   throw redirect(302, `/${lang}/events/`);
 };
